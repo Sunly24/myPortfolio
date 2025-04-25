e8d72027875245ec88cd03421978522dfa3c3ddc
Sunly <sonlychao169@gmail.com>
2025-04-25 21:18:12 +0700

    implement showcase-project

---
+++ src/sections/ShowcaseSection.jsx
@@ -0,0 +1,94 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const ShowcaseSection = () => {
+  const sectionRef = useRef(null);
+  const project1Ref = useRef(null);
+  const project2Ref = useRef(null);
+  const project3Ref = useRef(null);
+
+  useGSAP(() => {
+  const projects = [project1Ref.current, project2Ref.current, project3Ref.current];
+
+    projects.forEach((card, index) => {
+      gsap.fromTo(
+        card,
+        {
+          opacity: 0,
+          y: 50,
+        },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          delay: 0.3 * (index + 1),
+          scrollTrigger: {
+            trigger: card,
+            start: "top bottom-=100",
+          },
+          ease: "power2.inOut",
+        }
+      )
+    });
+
+    gsap.fromTo(sectionRef.current, 
+    {
+      opacity: 0,
+    },
+    {
+      opacity: 1,
+      duration: 1.5,
+    });
+  }, []);
+
+  return (
+    <section ref={sectionRef} id="work" className="app-showcase">
+      <div className="w-full">
+        <div className="showcaselayout">
+          {/* Left */}
+          <div className="first-project-wrapper" ref={project1Ref}>
+            <div className='image-wrapper'>
+              <img src="./images/project1.png" alt="Ryde" />
+            </div>
+            <div className="text-content">
+              <h2>
+                On-Demmad Ride Made Simple with a Powerful, User-Friendly App call Ryde
+              </h2>
+              <p className="text-white-50 md:text-xl">
+                Ryde is a cutting-edge on-demand ride-sharing app that revolutionizes the way people travel.
+              </p>
+            </div>
+          </div>
+
+          {/* Right */}
+          <div className="project-list-wrapper overflow-hidden">
+            <div className="project" ref={project2Ref}>
+              <div className="image-wrapper bg-[#ffefdb]">
+                <img src="./images/project2.png" alt="Library Management Platform" />
+              </div>
+              <h2>
+                Library Management Platform
+              </h2>
+            </div>
+
+            <div className="project" ref={project3Ref}>
+              <div className="image-wrapper bg-[#ffe7eb]">
+                <img src="./images/project3.png" alt="YC Directory" />
+              </div>
+              <h2>
+                YC Directory - A Startup Showcase App
+              </h2>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ShowcaseSection
