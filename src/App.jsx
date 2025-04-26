96fa0fdb9e07cfa24b41efc01f15de96a8a13da6
Sunly <sonlychao169@gmail.com>
2025-04-27 02:24:03 +0700

    logo_animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import NavBar from "./components/NavBar";
-const App = () => {
-  return (
+import LogoShowcase from "./sections/LogoShowcase";
+const App = () => (
     <>
     <NavBar />
     <Hero />
     <ShowcaseSection />
+    <LogoShowcase />
     </>
   );
-};
 
 export default App;
