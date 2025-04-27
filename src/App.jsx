f615c55902719ca72649f3c878c14f6079875cf6
Sunly <sonlychao169@gmail.com>
2025-04-27 12:46:33 +0700

    contact&footer site

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import LogoShowcase from "./sections/LogoShowcase";
 import FeatureCards from "./sections/FeatureCards";
 import Experience from "./sections/Experience";
 import TechStack from "./sections/TechStack";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
 const App = () => (
     <>
     <NavBar />
@@ -14,6 +16,8 @@ const App = () => (
     <FeatureCards />
     <Experience />
     <TechStack />
+    <Contact />
+    <Footer />
     </>
   );
 
